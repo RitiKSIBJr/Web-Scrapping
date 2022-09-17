2d787e905367ffbd39eee08c91e0b7428109e528
RitiKSiBJr <letter.ritik060@gmail.com>
2022-09-17 12:26:29 +0545

    seutp file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (
+    name = "Web Scrapping",
+    version = "1.0.0",
+    description = "Scrapping quotes and authors name from a website.",
+    author = "RitiK",
+    url = "https://github.com/RitikSibjr/Web-Scrapping"
+)
